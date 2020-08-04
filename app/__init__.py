ad5cdbbaf143f6c62a4df7fda3934a86cb286375
captain-booee <captain.booee@gmail.com>
2020-08-04 13:25:56 +0430

    register and login base design and control done! + user modle

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,25 @@
 from flask import Flask,render_template,flash,redirect,url_for
 from app.user.forms import RegistrationForm,LoginForm
-
+from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = "hello"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+    db = SQLAlchemy(app)
+
+    class User(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        email = db.Column(db.String(120), unique=True, nullable=False)
+        image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+        password = db.Column(db.String(60), nullable=False)
+
+        def __repr__(self):
+            return f"User('{self.email}','{self.image_file}')"
+
     @app.route('/')
     def home():
         return render_template('home.html')
