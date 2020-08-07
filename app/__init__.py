2f8e846ae13eb423621f9d99b02171fffd9173b7
captain-booee <captain.booee@gmail.com>
2020-08-07 12:28:13 +0430

    login and register done
    *most likely due to a circular import ERROR

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,18 @@
 from flask import Flask,render_template,flash,redirect,url_for
 from app.init import db
 from app.packages import users
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
+app = Flask(__name__)
+login_manager = LoginManager(app)
+bcrypt = Bcrypt(app)
 
-def create_app():
-    app = Flask(__name__)
+app.config['SECRET_KEY'] = "hello"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-    app.config['SECRET_KEY'] = "hello"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
-    db.init_app(app, )
-    app.register_blueprint(users.bp)
-    return app
+db.init_app(app, )
+app.register_blueprint(users.bp)
 
 # need to :
 #from app import db
--- app/packages/users/__init__.py
+++ app/packages/users/__init__.py
@@ -2,10 +2,11 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from .models import User, Contact
 from app.init import db
 from .forms import LoginForm, RegistrationForm
-from flask_bcrypt import Bcrypt
+
+from app import bcrypt
+from flask_login import login_user
 
 bp = Blueprint("users", __name__, template_folder="templates/")
-bcrypt = Bcrypt()
 
 
 @bp.route('/')
@@ -29,6 +30,10 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+            login_user(user, remember=form.remember.data)
+
         #flash(f'Account created for {form.email.data}', 'success')
         return redirect(url_for('users.home'))
     return render_template('login.html', form=form)
